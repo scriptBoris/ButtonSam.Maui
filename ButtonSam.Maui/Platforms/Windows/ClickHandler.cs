bf75ad63914bd47af92d03d8d04ead66a1b29aaa
Boris Nimaev <scriptboris@gmail.com>
2023-04-07 10:15:06 +0600

    - Added windows placeholder handlers

---
+++ ButtonSam.Maui/Platforms/Windows/ClickHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui.Handlers;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButtonSam.Maui.Internal
+{
+    public partial class ClickHandler : ViewHandler<Click, Microsoft.UI.Xaml.Controls.Viewbox>
+    {
+        protected override Viewbox CreatePlatformView()
+        {
+            var n = new Viewbox();
+            return n;
+        }
+    }
+}
--- ButtonSam.Maui/Platforms/Windows/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ButtonSam.Maui
-{
-    // All the code in this file is only included on Windows.
-    public class PlatformClass1
-    {
-    }
-}
